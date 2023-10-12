addd87b6e2ed0237d5ae37080e5da7711217f6da
Your Name <you@example.com>
2023-10-13 00:26:07 +0300

    File 03.js: fixed.

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,2 +1,6 @@
 let number = 1;
-console.log(number++ + "," + ++number + "," + number-- + "," + --number);
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number);
+
