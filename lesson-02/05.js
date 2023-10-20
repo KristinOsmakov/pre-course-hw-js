1386973d19d4275f5f21cad6be0fc10eaf081b0e
Your Name <you@example.com>
2023-10-20 23:08:49 +0300

    Fixed code in file 05.js.

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -2,7 +2,10 @@ let passport = {
     name: "Petr",
     surname: "Petrov",
 };
-let a = passport;
+let a = {};
+for (let key in passport) {
+    a[key] = passport[key];
+}
 a.name = "Ivan";
 console.log(passport);
 console.log(a);
