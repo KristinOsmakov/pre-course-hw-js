51f89cbd27c2e2d2950f81298dbf810ba8b596a5
Your Name <you@example.com>
2023-10-27 00:34:18 +0300

    Add file 01.js in lesson 3.

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,9 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    const newColors = colors.filter(el => el === "черный" || el === "красный" || el === "желтый");
+    return newColors.join("-");
+}
+
+console.log(createColorString());
+
