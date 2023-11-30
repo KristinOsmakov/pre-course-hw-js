704483b67dd92eb3f5cad82fab9ff8a57b938060
Kris <you@example.com>
2023-11-30 18:16:17 +0300

    Edit file 07.js. Not comment.

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -1,10 +1,10 @@
-// const numbers = [
-//     [1, 2, 3, 4, 5],
-//     [11, 20, 33, 40, 55],
-//     [111, 200, 333, 400, 555],
-// ];
-//
-//
-//
-// console.log(numbers.map(row => row.filter(el => el % 2 === 0)));
-//
+const numbers = [
+    [1, 2, 3, 4, 5],
+    [11, 20, 33, 40, 55],
+    [111, 200, 333, 400, 555],
+];
+
+
+
+console.log(numbers.map(row => row.filter(el => el % 2 === 0)));
+
