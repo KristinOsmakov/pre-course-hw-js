fadbaea8e4f00fd9b085b9872c16fd08c517a52a
Kris <you@example.com>
2023-11-30 19:30:17 +0300

    Fixed file 07.js.

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -1,12 +1,12 @@
-// let passportMarried = {
-//     name: "Petr",
-//     surname: "Petrov",
-//     address: {
-//         country: "USA",
-//         city: "Bobryisk"
-//     }
-// };
-// const passportMarried2 = {...passportMarried, married:true, address:{...passportMarried.address}};
-// passportMarried2.married = true;
-// console.log(passportMarried);
-// console.log(passportMarried2);
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+const passportMarried2 = {...passportMarried, married:true, address:{...passportMarried.address}};
+passportMarried2.married = true;
+console.log(passportMarried);
+console.log(passportMarried2);
